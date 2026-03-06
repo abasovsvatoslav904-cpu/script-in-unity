7007f86430c7e2d589405a59572ea98ac7c02fec
abasovsvatoslav904-cpu <abasovsvatoslav904@gmail.com>
2026-03-06 20:22:13 +0300

    1 script

---
+++ HelloyWorld.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class HelloyWorld : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print("Helloy World"); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
