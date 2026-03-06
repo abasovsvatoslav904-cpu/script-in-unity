13270c9a2668c0e11dc70beca2843ccaf7671f0b
abasovsvatoslav904-cpu <abasovsvatoslav904@gmail.com>
2026-03-06 20:48:43 +0300

    work

---
+++ Criked.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Criked : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print("criked");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("criked");
+    }
+}
